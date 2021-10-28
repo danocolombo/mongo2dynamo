e6f235242ee127f17d21aa4ac29b6aa9df64e752
danocolombo <danocolombo@gmail.com>
2021-10-28 05:55:21 -0400

    peoples conversion complete

--- main.py
+++ main.py
@@ -19,7 +19,11 @@ import m2d_humans_2_remove_mongo_data_types
 import m2d_humans_3_add_dynamodb_data_types
 import m2d_humans_4_rename_fields
 import m2d_humans_5_aws_wrap_each_meeting
-
+import m2d_peoples_1_json_files
+import m2d_peoples_2_remove_mongo_data_types
+import m2d_peoples_3_add_dynamodb_data_types
+import m2d_peoples_4_rename_fields
+import m2d_peoples_5_aws_wrap_each_meeting
 # DO MEETINGS
 try:
     # convert the Meetings data
@@ -132,6 +136,37 @@ try:
                 if m2d_humans_4_rename_fields.change_field_names():
                     print(f"4. Dynamo fields renamed")
                     if m2d_humans_5_aws_wrap_each_meeting.dynamodb_wrapper():
+                        print(f"5. Dynamo wrappers applied")
+                    else:
+                        print(f"dynamodb_wrapper(): error")
+                else:
+                    print(f"change_field_names(): error")
+            else:
+                print(f"add_dynamo_data_types(): error")
+        else:
+            print(f"ERROR REMOVING DATA TYPES")
+    else:
+        raise Exception('create_json_compliant_files(): error')
+
+except Exception as err:
+    print(f"{err}")
+
+finally:
+    print("mongo2dynamo: Humans converted and ready")
+
+# DO PEOPLES
+try:
+    # convert the Peoples data
+    if m2d_peoples_1_json_files.create_json_compliant_files():
+        print(f"1. mongo data transformed to json [aws-ready-files produced]")
+        # strip out the mongo data types
+        if m2d_peoples_2_remove_mongo_data_types.remove_mongo_data_types():
+            print(f"2. Mongo data types removed")
+            if m2d_peoples_3_add_dynamodb_data_types.add_dynamo_data_types():
+                print(f"3. Dynamo data types added")
+                if m2d_peoples_4_rename_fields.change_field_names():
+                    print(f"4. Dynamo fields renamed")
+                    if m2d_peoples_5_aws_wrap_each_meeting.dynamodb_wrapper():
                         print(f"5. Dynamo wrappers applied")
                     else:
                         print(f"dynamodb_wrapper(): error")
