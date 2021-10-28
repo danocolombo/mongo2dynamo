3a5503b7ef40cf62955565a37c3fef8b7c378ed7
danocolombo <danocolombo@gmail.com>
2021-10-28 05:24:04 -0400

    humans conversion complete

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ import m2d_clients_2_remove_mongo_data_types
 import m2d_clients_3_add_dynamodb_data_types
 import m2d_clients_4_rename_fields
 import m2d_clients_5_aws_wrap_each_meeting
+import m2d_humans_1_json_files
+import m2d_humans_2_remove_mongo_data_types
+import m2d_humans_3_add_dynamodb_data_types
+import m2d_humans_4_rename_fields
+import m2d_humans_5_aws_wrap_each_meeting
 
 # DO MEETINGS
 try:
@@ -113,3 +118,34 @@ except Exception as err:
 
 finally:
     print("mongo2dynamo: Clients converted and ready")
+
+# DO HUMANS
+try:
+    # convert the Humans data
+    if m2d_humans_1_json_files.create_json_compliant_files():
+        print(f"1. mongo data transformed to json [aws-ready-files produced]")
+        # strip out the mongo data types
+        if m2d_humans_2_remove_mongo_data_types.remove_mongo_data_types():
+            print(f"2. Mongo data types removed")
+            if m2d_humans_3_add_dynamodb_data_types.add_dynamo_data_types():
+                print(f"3. Dynamo data types added")
+                if m2d_humans_4_rename_fields.change_field_names():
+                    print(f"4. Dynamo fields renamed")
+                    if m2d_humans_5_aws_wrap_each_meeting.dynamodb_wrapper():
+                        print(f"5. Dynamo wrappers applied")
+                    else:
+                        print(f"dynamodb_wrapper(): error")
+                else:
+                    print(f"change_field_names(): error")
+            else:
+                print(f"add_dynamo_data_types(): error")
+        else:
+            print(f"ERROR REMOVING DATA TYPES")
+    else:
+        raise Exception('create_json_compliant_files(): error')
+
+except Exception as err:
+    print(f"{err}")
+
+finally:
+    print("mongo2dynamo: Humans converted and ready")
